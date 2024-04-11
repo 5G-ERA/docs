c2103fe5da63c080e7a7a7a181a576aaa267cce1
Bartosz Bratuś <34897359+Artonus@users.noreply.github.com>
2024-04-11 16:51:55 +0200

    fix: optimized middleware architecture diagram

--- Middleware/diagrams/middleware-diagrams.py
+++ Middleware/diagrams/middleware-diagrams.py
@@ -1,9 +1,11 @@
 from diagrams import Diagram, Cluster
 from diagrams.aws.network import NLB
+from diagrams.aws.compute import EC2
 
 from diagrams.custom import Custom
 from diagrams.k8s.compute import Deploy
 from diagrams.k8s.network import Ingress
+from diagrams.onprem.network import Nginx
 from diagrams.onprem.database import InfluxDB
 from diagrams.onprem.inmemory import Redis
 from diagrams.onprem.monitoring import Grafana
@@ -11,35 +13,27 @@ from diagrams.onprem.queue import RabbitMQ
 
 
 
-with Diagram("Architecture", direction="LR", show=False, filename="crop-architecture"):
+with Diagram("Architecture", direction="TB", show=False, filename="crop-architecture"):
     robots = Custom("Robot", "../img/summit-xl.jpg")
     #admin = User("Admin")
-    nlb = NLB("NLB")
-    with Cluster("Infrastructure") as infra:
-        redis = Redis("Redis")
-        rabbitmq = RabbitMQ("RabbitMQ")
-        grafana = Grafana("Grafana")
-        influx = InfluxDB("InfluxDB")
+    #nlb = NLB("NLB")
+    with Cluster("AWS Cluster", direction="TB") as infra:
+        with Cluster("Services") as services:
+            redis = Redis("Redis")
+            rabbitmq = RabbitMQ("RabbitMQ")
+            grafana = Grafana("Grafana")
+            influx = InfluxDB("InfluxDB")
+        with Cluster("Middleware", direction="TB") as middleware:
+            gateway = Nginx("Gateway")
+            mw = EC2("Middleware services")
+            orchestrator = EC2("Orchestrator")
+            gateway >> mw >> orchestrator
+            mw >> [redis, influx, rabbitmq]
+        with Cluster("Network Applications") as net_apps:
+            netapps = [EC2("Object Detection"), EC2("SLAM"), EC2("Teleoperation")]
 
-    with Cluster("CentralApi") as central_api:
-        central_api_pod = Deploy("CentralApi")
-        dashboard = Deploy("Dashboard")
-        central_api_pod >> redis
-
-    with Cluster("Middleware") as middleware:
-        gateway = Ingress("Gateway")
-        redis_interface = Deploy("RedisInterface")
-        orchestrator = Deploy("orchestrator")
-        task_planner = Deploy("TaskPlanner")
-        resource_planner = Deploy("ResourcePlanner")
-        pods = [redis_interface, orchestrator, task_planner, resource_planner]
-        gateway >> pods
-        redis_interface >> redis
-        orchestrator >> influx
-        [orchestrator, task_planner, resource_planner] >> rabbitmq
-        [orchestrator, task_planner, resource_planner] >> redis_interface
-    with Cluster("Network Applications") as net_apps:
-        netapp = Deploy("Network Application")
-        pass
-    gateway >> netapp
-    robots >> nlb >> [central_api_pod, gateway, grafana, dashboard]
+            pass
+    gateway >> netapps
+    #robots >> nlb >> [gateway]
+    robots >> [gateway]
+    #orchestrator >> [netapp, netapp2]
